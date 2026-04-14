1f02bef421598db0a816ba75a245773926e7ebd5
kevvhutovskyi <volodymyr.hutovskyi@kevychsolutions.com>
2026-04-14 21:49:32 +0300

    Updated index.ts bull board init

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,22 @@ import { testApi } from "./modules/test-api";
 import { serverAdapter } from "./bullmq";
 import { openapi } from '@elysiajs/openapi';
 
-const app = new Elysia();
+async function bootstrap() {
+  const app = new Elysia();
+  
+  const bullBoardPlugin = await serverAdapter.registerPlugin();
 
-app
-  .use(serverAdapter.registerPlugin())
-  .use(testApi)
-  .use(openapi());
+  app
+    .use(bullBoardPlugin)
+    .use(testApi)
+    .use(openapi());
 
-app.listen(3000);
+  app.listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  console.log(
+    `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  );
+}
+
+bootstrap();
+
