120de7f8b281677b6b95809adf78eb7242d29db9
kevvhutovskyi <volodymyr.hutovskyi@kevychsolutions.com>
2026-04-15 00:00:17 +0300

    Add types to test quyer GET endpoint

--- src/modules/test-api/index.ts
+++ src/modules/test-api/index.ts
@@ -1,7 +1,9 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import { exampleBullMq } from "../../bullmq";
 
 export const testApi = new Elysia({ prefix: 'api' })
     .get('test', async ({ query }) => {
         exampleBullMq.add('BullMQ', { title: query.title });
-    })
+    }, { query: t.Object({
+        title: t.String()
+    }) })
