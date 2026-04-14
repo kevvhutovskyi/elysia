80cc8966b52a30c70b063b7ff03acec6b0f15000
kevvhutovskyi <volodymyr.hutovskyi@kevychsolutions.com>
2026-04-14 23:50:14 +0300

    Update port 80

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import { Elysia } from "elysia";
 import { testApi } from "./modules/test-api";
 import { serverAdapter } from "./bullmq";
 import { openapi } from '@elysiajs/openapi';
+import cors from '@elysiajs/cors';
 
 async function bootstrap() {
   const app = new Elysia();
-  
+
   const bullBoardPlugin = await serverAdapter.registerPlugin();
 
   app
+    .use(cors())
     .use(bullBoardPlugin)
     .use(testApi)
     .use(openapi());
